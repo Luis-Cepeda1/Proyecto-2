7a7c5c0f7b3bf5cde8ef95121caed41ebfd96f76
LuisJose-Correa <ljcorrea@miuandes.cl>
2026-03-21 19:44:07 -0300

    mejora de output

--- main.py
+++ main.py
@@ -1,38 +1,41 @@
 import sys
 
 tareas = []
-with open("tareas.txt") as f:
+with open("tareas_2.txt") as f:
     for linea in f.readlines():
         partes = linea.strip().split(",")
         tareas.append((partes[0], int(partes[1]), partes[2]))
 
 recursos = []
-with open("recursos.txt") as f:
+with open("recursos_2.txt") as f:
     for linea in f.readlines():
         partes = linea.strip().split(",")
         recursos.append((partes[0], set(partes[1:])))
 
-tareas_ordenadas = sorted(tareas, key=lambda t: t[1], reverse=True)
+tareas = sorted(tareas, key=lambda t: t[1], reverse=True)
 
-tiempo_recurso = {}
+tiempo_libre = {}
 for rid, cats in recursos:
-    tiempo_recurso[rid] = 0
+    tiempo_libre[rid] = 0
 
 resultado = []
 
-for tarea_id, duracion, categoria in tareas_ordenadas:
+for tarea_id, duracion, categoria in tareas:
     mejor_recurso = None
     menor_tiempo = 999999
+
     for rid, cats in recursos:
         if categoria in cats:
-            if tiempo_recurso[rid] < menor_tiempo:
-                menor_tiempo = tiempo_recurso[rid]
+            if tiempo_libre[rid] < menor_tiempo:
+                menor_tiempo = tiempo_libre[rid]
                 mejor_recurso = rid
-    inicio = tiempo_recurso[mejor_recurso]
+
+    inicio = tiempo_libre[mejor_recurso]
     fin = inicio + duracion
-    tiempo_recurso[mejor_recurso] = fin
+    tiempo_libre[mejor_recurso] = fin
     resultado.append((tarea_id, mejor_recurso, inicio, fin))
 
 with open("output.txt", "w") as f:
     for tarea_id, rid, inicio, fin in resultado:
-        f.write(tarea_id + "," + rid + "," + str(inicio) + "," + str(fin) + "\n")
+      f.write(f"{tarea_id},{rid},{inicio},{fin}\n")
+      
